154537d016d2ed216dc5410503475c12ee64f32d
Sourav Sudhakaran <souravssudhakaran@gmail.com>
2026-02-23 21:29:01 +0000

    fix bugs

--- generatre_cases.py
+++ generatre_cases.py
@@ -1,9 +1,9 @@
 from taskManager import OpenFOAMCaseGenerator
 
 generator = OpenFOAMCaseGenerator(
-    template_path="/home/sourav/CFD_Dataset/openfoam_caseGenerator/template",
-    input_dir="/home/sourav/CFD_Dataset/generateInputs/Data_test/downloads",
-    output_dir="/home/sourav/CFD_Dataset/openFoamCases"
+    template_path="/home/sourav/1_CFD_Dataset/openfoam_caseGenerator/template",
+    input_dir="/home/sourav/1_CFD_Dataset/generateInputs/Data/downloads",
+    output_dir="/home/sourav/1_CFD_Dataset/1_Data"
 )
 
 generator.generate_all_cases()
